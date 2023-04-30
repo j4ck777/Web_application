2426aa37f14eda2925ccb52ce7fe07840c2adda8
Fakhriddin <s317885@studenti.polito.it>
2023-04-30 17:43:26 +0200

    started

---
+++ Demo2.js
@@ -0,0 +1,13 @@
+"use strict";
+ a='abc';
+console.log(a);
+
+var a=3;
+console.log(a);
+for(let i=0; i<10; i++){
+    console.log(i);
+}
+let seq= [2,3,56,2,446, 'fcsdv'];
+console.log(seq.length);
+let copy=Array.from(seq);
+console.log(copy)
