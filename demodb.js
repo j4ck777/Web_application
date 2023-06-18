258a58f5f30bd9aeba381820744fc4353ee0cf5a
Fakhriddin <s317885@studenti.polito.it>
2023-06-18 15:07:02 +0200

    UPDATE SQL JS

--- demodb.js
+++ demodb.js
@@ -5,3 +5,21 @@ const sqlite = require('sqlite3');
 const db= new sqlite.Database('questions.sqlite', (err)=>{
     if(err)throw err;
 });
+ 
+
+const sql ='SELECT * FROM answer';
+
+let authorList=[];
+
+
+db.all(sql , (err, rows)=>{
+    if(err)
+        throw err;
+    else{
+        console.log(rows);
+        authorList=rows.map((item)=>item['author']);
+        console.log(authorList);
+    }
+});
+console.log("author list out site:");
+console.log(authorList);
